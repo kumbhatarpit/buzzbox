26ffe4fe8124d58ef451f93e99bbaf57c5781d3f
huner.sharma <huner.sharma@nsi.lab>
2025-06-08 20:29:19 +0530

    Home and pricing changes

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ArrowRight, Award, Briefcase, CalendarDays, DollarSign, Clock, Download, Film, GraduationCap, Image, LucideAngularModule, LucideIconData, Palette, PlayCircle, RefreshCw, Rocket, Store, Type, Users } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [
+    RouterModule,
+    LucideAngularModule
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  download = Download;
+  arrowRight = ArrowRight;
+  playCircle = PlayCircle;
+  users = Users;
+  rocket = Rocket;
+  briefcase = Briefcase;
+  graduationCap = GraduationCap;
+  refreshCw = RefreshCw;
+  store = Store;
+  film = Film;
+  image = Image;
+  type = Type;
+  calendarDays = CalendarDays;
+  clock = Clock;
+  award = Award;
+  palette = Palette;
+  dollarSign = DollarSign;
+}
--- client/src/app/pricing/pricing.component.ts
+++ client/src/app/pricing/pricing.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, CheckCircle } from 'lucide-angular';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.css']
 })
-export class PricingComponent {}
+export class PricingComponent {
+  check = CheckCircle;
+}
